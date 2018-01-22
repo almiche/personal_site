bb17362b4787466f9aa364e742d2878fdf0fc6d1
almiche <chamich196@hotmail.com>
2018-01-22 02:44:36 -0500

    Created page factory in order to make website easily maintainable

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 
 
 $(document).ready(function(){
+
+    //Center to sidebar animation
     $('.navbar.navbar-inverse li').click(function(){
 
        $(this).toggleClass('active'); 
@@ -21,52 +23,33 @@ $(document).ready(function(){
             fontSize:'1em'
         },1000);
 
-    $('vertical_menu li').click(function(){
-        $(this).css()
-    });
-    
     });
 
-    
     $('.selection,.list-group-item').click(function(){
-        request = new XMLHttpRequest();
-        request.onreadystatechange = function()
-        {
-            if (request.readyState == 4 && request.status == 200)
-            {
-                callback(request.responseText); // Another callback here
-            }
-        }; 
-        switch($(this).text()) {
-            case "Home":
-                request.open("GET", "/", true);
-                break;
-            case "Projects":
-                request.open("GET", "/projects", true);
-                 break;
-            case "About Me":
-                request.open("GET", "/aboutme", true);
-                break;
-            case "Resume":
-                request.open("GET", "/resume", true);
-                break;
-        }
+        pageFactory( $(this).attr('value'), callback);
 
-        request.send();
     });
+
+    function pageFactory(pageName,callback){
+        
+        request = new XMLHttpRequest();
+            request.onreadystatechange = function()
+            {
+                if (request.readyState == 4 && request.status == 200)
+                {
+                    callback(request.responseText); 
+                }
+            }; 
+        
+        request.open("GET", "/" + pageName, true);
+        request.send();
+    }
+        
 
     function callback(response){
         $("#projects_selection").fadeOut('slow');
         $("#centerpiece").empty().append(response);
         $("#projects_selection:hidden:first").fadeIn('slow');
     }
-
-
-    function vertMenu(){
-
-    }
-
-    
-
 
 });
